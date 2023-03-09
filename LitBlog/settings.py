471f4d7b9b4bcedec29527a19daf2ae053ec4c07
zoyku <108931341+zoyku@users.noreply.github.com>
2023-03-09 10:39:00 +0300

    Update settings.py

--- LitBlog/settings.py
+++ LitBlog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--^924ivh-%a7jca=ng74v^t74#fe$+dyi&yklkk43wrcu+f8!j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
